7c748483aa661077006868d584a5e5141bda10bf
SJH-00 <tlstkd437@gmail.com>
2024-09-30 01:40:50 +0900

    swagger setting

---
+++ src/main/java/capstone/petmily/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package capstone.petmily.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI PetmilyAPI() {
+        Info info = new Info()
+                .title("Inha capstone petmily API")
+                .description("Inha capstone petmily API 명세서")
+                .version("1.0.0");
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info);
+    }
+}
